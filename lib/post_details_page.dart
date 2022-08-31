9d026ed68d6a0c69b2ab5d30efddec61df08f7a2
eRvshiEld <ervine.hinay@gmail.comm>
2022-08-31 15:50:45 +0800

    post details page changes

--- lib/post_details_page.dart
+++ lib/post_details_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:lookal/widget/header_widget.dart';
 
 class PostDetailsPage extends StatelessWidget {
@@ -9,6 +10,8 @@ class PostDetailsPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: EdgeInsets.all(20),
@@ -27,6 +30,44 @@ class PostDetailsPage extends StatelessWidget {
                     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoM1mYfFL8CRDMNxUUKppwR95Im6NW2c3lg0qRHKvwrdV43nDC4hPpX3pSfaQJE1jV1uU&usqp=CAU",
                   ),
                 ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Cabbage",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.pesoSign,
+                        size: 20,
+                      ),
+                      Text(
+                        "20",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Icon(
+                    Icons.local_attraction_rounded,
+                  ),
+                ],
               ),
             ),
           ],
